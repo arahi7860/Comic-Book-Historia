9215d23a92ee4a256eccc4db6d20b8c7d6167282
arahi7860 <54910341+arahi7860@users.noreply.github.com>
2023-02-08 21:08:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-// 20 Questions Trivia URL Random Questions Comics
-// const URL = "https://opentdb.com/api.php?amount=20&category=29";
-
 const URL = "https://opentdb.com/api.php?amount=1&category=29&type=multiple";
-// let QUESTION = 1;
-// let data;
-// 15 Questions Trivia URL
-// const URL =
-//   "https://opentdb.com/api.php?amount=15&category=29&difficulty=hard&type=multiple";
-
-// Another 15 questions Trivia URL
-// const URL =
-//   "https://opentdb.com/api.php?amount=15&category=29&difficulty=hard&type=multiple";
 
 const _question = document.querySelector("#question");
 const _options = document.querySelector(".quiz-options");
@@ -90,6 +78,7 @@ const showQuestions = (json) => {
   chooseOptions();
 };
 
+// StackOverFlow Javascript Quotation problem
 const decode = (str) => {
   let txt = document.createElement("textarea");
   txt.innerHTML = str;
@@ -159,123 +148,3 @@ const quizAgain = () => {
   settingCount();
   getQuestions();
 };
-// getQuestions();
-// Create An Array to hold all the answers
-// Once the user picks a choice compare it to an array of right answers
-// If there is a match then it's correct if not it's incorrect
-// const displayMessage = function (message) {
-//   document.querySelector(".message").textContent = message;
-// };
-
-// let arr = [];
-// async function addQuestions() {
-//   const prompt = await getQuestions();
-
-//   let i = 1;
-//   let answer = false;
-//   // for (const layout of prompt) {}
-//   for (const layout of prompt) {
-//     // if(layout.question)
-//     const p = document.createElement("p");
-//     p.textContent = layout.question;
-//     const form = document.querySelector("form");
-//     form.append(p);
-//     p.classList.add(`question`);
-
-//     document.querySelectorAll("p").forEach((p) => {
-//       p.innerText.replace(/(&#(\d+);)/g, (match, capture, charCode) =>
-//         String.fromCharCode(charCode)
-//       );
-//     });
-
-//     console.log(p);
-
-//     let answers = [];
-//     answers.push(layout.correct_answer);
-//     for (let i = 0; i < layout.incorrect_answers.length; i++) {
-//       answers.push(layout.incorrect_answers[i]);
-//     }
-
-//     const getShuffledArr = (arr) => {
-//       const newArr = arr.slice();
-//       for (let i = newArr.length - 1; i > 0; i--) {
-//         const rand = Math.floor(Math.random() * (i + 1));
-//         [newArr[i], newArr[rand]] = [newArr[rand], newArr[i]];
-//       }
-//       return newArr;
-//     };
-//     const shuffledAnswers = getShuffledArr(answers);
-//     // console.log(shuffledAnswers);
-
-//     const newForm = (counter) => {
-//       console.log(counter);
-//       for (let i = 0; i < shuffledAnswers.length; i++) {
-//         const input = document.createElement("input");
-//         const label = document.createElement("label");
-//         // console.log("newForm");
-//         input.type = "radio";
-//         input.id = `choices-${i + 1}`;
-//         input.name = `question-${counter}`;
-//         label.htmlFor = `choices-${i + 1}`;
-
-//         // Store all of your data in a variable that includes all questions and answers
-//         // Create a for loop that goes through the data and it outputs the question
-//         // one at a time
-
-//         label.innerHTML = shuffledAnswers[i];
-//         form.append(input, label);
-
-//         console.log(input);
-//         console.log(label);
-//         // console.log(`answers: ${shuffledAnswers.length}`);
-//         // console.log(`index: ${i}`);
-
-//         // if (i > shuffledAnswers.length) {
-//         //   input.type = "submit";
-//         // }
-//       }
-//     };
-//     newForm(i);
-//     i++;
-
-//     // const checkAnswer = () => {
-//     //       let checkButton = true;
-//     //       if(document.querySelector());
-//     // };
-//     // checkAnswer();
-//   }
-
-//   const button = document.createElement("input");
-//   button.type = "submit";
-//   button.name = "enter";
-//   document.querySelector("form").append(button);
-// }
-
-// addQuestions();
-
-// // const userPrompt = async () => {
-// //   const add = await addQuestions();
-
-// //   add.map(() => {});
-// // };
-// // for (let i = 0; i < shuffledAnswers.length; i++) {
-// //   if (
-// //     document.querySelector(`#choices-${i}`) === layout.correct_answer
-// //   ) {
-// //     displayMessage("Correct!");
-// //   } else if (
-// //     document.querySelector(`choices-${i}`) !== layout.correct_answer
-// //   ) {
-// //     displayMessage("Incorrect!");
-// //   }
-// // }
-
-// console.log(json.results);
-// const qa = json.results.map(
-//   ({ question, correct_answer, incorrect_answers }) => ({
-//     question,
-//     correct_answer,
-//     incorrect_answers,
-//   })
-// );
-// console.log(qa)
